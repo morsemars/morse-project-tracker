23573ceda0e0c52d92756fe5375685ad31ebd84c
Marcelino Madriaga <madriaga.marcelino.jr@gmail.com>
2020-04-06 13:11:22 +0800

    FIX: use database_path parameter
    PROBLEM:
    The tests are not conecting to test databse.
    
    Solution:
    Modify create_app to accept a database_path

--- mpt/__init__.py
+++ mpt/__init__.py
@@ -3,10 +3,11 @@ from flask_cors import CORS
 from mpt.models import setup_db
 from mpt.views import setup_views
 
-def create_app(test_config=None):
+def create_app(test_config=None, database_path=None):
     app = Flask(__name__)
+    app.secret_key = "SECRET HERE"#TODO: Transfer to separate file
     setup_views(app)
-    setup_db(app)
+    setup_db(app, database_path)
     CORS(app)
 
     return app
