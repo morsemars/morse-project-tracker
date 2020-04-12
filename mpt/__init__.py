1e88ef338ebce4a76431c7da268f46f005834ed0
Marcelino Madriaga <madriaga.marcelino.jr@gmail.com>
2020-04-12 16:44:09 +0800

    FIX: add default database path

--- mpt/__init__.py
+++ mpt/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_cors import CORS
-from mpt.models import setup_db
+from mpt.models import setup_db, DEFAULT_DB_PATH
 from mpt.views import setup_views
 
-def create_app(test_config=None, database_path=None):
+def create_app(test_config=None, database_path=DEFAULT_DB_PATH):
     app = Flask(__name__)
     app.secret_key = "SECRET HERE"#TODO: Transfer to separate file
     setup_views(app)
