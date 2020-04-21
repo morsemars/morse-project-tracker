007b204dd0f7889adc72e94b9c96041c169f4f52
Marcelino Madriaga <madriaga.marcelino.jr@gmail.com>
2020-04-21 11:23:35 +0800

    FIX: database path
    use database path set on environment

--- mpt/__init__.py
+++ mpt/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
-from mpt.models import setup_db, DEFAULT_DB_PATH
+from mpt.models import setup_db
 from mpt.views import setup_views
+import os
+
+DEFAULT_DB_PATH = "postgresql://postgres@localhost:5433/project_tracker"
+
+if "DATABASE_URL" in os.environ:
+    DEFAULT_DB_PATH = os.environ["DATABASE_URL"]
 
 def create_app(test_config=None, database_path=DEFAULT_DB_PATH):
     app = Flask(__name__)
--- mpt/models/__init__.py
+++ mpt/models/__init__.py
@@ -1,14 +1,8 @@
-import os
 from flask_sqlalchemy import SQLAlchemy
 
-DEFAULT_DB_PATH = "postgresql://postgres@localhost:5433/project_tracker"
-
-if "DATABASE_URL" in os.environ:
-    database_path = os.environ["DATABASE_URL"]
-    
 db = SQLAlchemy()
 
-def setup_db(app, database_path=DEFAULT_DB_PATH):
+def setup_db(app, database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
