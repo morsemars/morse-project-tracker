bdd51c32fef30c8e55e2940ef4b7e66a65cdce8f
Marcelino Madriaga <madriaga.marcelino.jr@gmail.com>
2020-04-27 15:23:31 +0800

    REFACTOR: follow pep 8 style guide

--- mpt/models/user.py
+++ mpt/models/user.py
@@ -11,11 +11,13 @@ class User(base_model):
     last_name = Column(String(50), nullable=False)
     position = Column(String(15), nullable=False)
     tasks = db.relationship(
-        "Task", cascade="all,
-        delete-orphan", lazy=True
+        "Task",
+        cascade="all, delete-orphan",
+        lazy=True
     )
     projects = db.relationship(
-        "Project", secondary=project_assignment,
+        "Project",
+        secondary=project_assignment,
         lazy=True
     )
 
